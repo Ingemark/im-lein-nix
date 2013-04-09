bee6a3a0a26d9ffba9435dc84eb0228c269d8407
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-04-09 10:16:01 +0200

    Eat our own dogfood: release with lein-nix

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "https://github.com/Inge-mark/lein-nix"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in :leiningen)
+  :eval-in :leiningen
+  :aliases {"release" ["xdo" "git-check-clean,"
+                       "thrush" "version-update" ":release," "edit-version,"
+                       "xdo" "deploy" "clojars," "commit" "New release," "tag,"
+                       "thrush" "version-update" ":new-snapshot," "edit-version,"
+                       "xdo" "commit" "New snapshot," "push"]})
