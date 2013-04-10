980d53905d800cf97a395ee3be5d223acd75d4a4
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-04-10 22:09:14 +0200

    New release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-nix "0.1.6-SNAPSHOT"
+(defproject lein-nix "0.1.6"
   :description "An arsenal of composable bundling/deployment tasks"
   :url "https://github.com/Inge-mark/lein-nix"
   :license {:name "Eclipse Public License"
