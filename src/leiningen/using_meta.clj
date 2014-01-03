3e803bd6d328518487398f5766a8a02ecc39a131
Marko Topolnik <mtopolnik@inge-mark.hr>
2014-01-03 12:39:11 +0100

    Fix and document using-meta

--- src/leiningen/using_meta.clj
+++ src/leiningen/using_meta.clj
@@ -1,5 +1,13 @@
 (ns leiningen.using-meta
   (require [leiningen.core.main :as main]))
 
-(defn ^:higher-order using-meta [project key & cmdline]
-  (main/resolve-and-apply project (concat cmdline ((meta project) key))))
+(defn ^:higher-order using-meta
+  "Invoke a task using project map's metadata
+
+Retrieves the collection under key in project map's metadata, concatenates it
+onto task-call, and evaluates the resulting form as a leiningen task invocation.
+Designed to be used in conjunction with into-meta, which prepares the collection in
+project map's metadata."
+  [project key & task-call]
+  (main/resolve-and-apply
+    project (concat task-call ((meta project) (if (.startsWith key ":") (read-string key) key)))))
