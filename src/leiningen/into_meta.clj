eb1a1ad9e8855e0561d51e47f961190aa3736e1b
Marko Topolnik <mtopolnik@inge-mark.hr>
2014-01-03 12:39:18 +0100

    Document into-meta

--- src/leiningen/into_meta.clj
+++ src/leiningen/into_meta.clj
@@ -1,6 +1,11 @@
 (ns leiningen.into-meta)
 
-(defn into-meta [project value key & more-values]
+(defn into-meta
+  "Add values to project map's metadata
+
+Modifies the project map's metadata by appending (concat [value] more-values) to the
+collection under key, creating one if absent."
+  [project value key & more-values]
   (let [key (if (.startsWith key ":") (read-string key) key)
         values (map #(if (map? %) (first (vals %)) %) (concat [value] more-values))]
     ^:boxed-result
